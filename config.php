4cb4aa771296a4eff309db919a9f4429a407b781
00realunix <realunix00@gmail.com>
2026-08-01 12:09:47 +0800

    Add initial config.php with Magento license details
    This file contains license information and configuration settings for Magento.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an note to
+ * license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://magento.com for more information.
+ *
+ * @category    Magento
+ * @package     Magento_Includes
+ * @copyright   Copyright (c) 2006-2015 X.commerce, Inc. (http://magento.com)
+ * @license     http://opensource.org  Academic Free License (AFL 3.0)
+ */
+
+#define('COMPILER_INCLUDE_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR.'src');
+#define('COMPILER_COLLECT_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR.'stat');
