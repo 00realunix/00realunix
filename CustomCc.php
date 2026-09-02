bfb2d282e71d9836dd066f5344707dfebfd2834a
00realunix <realunix00@gmail.com>
2026-09-03 00:03:15 +0800

    Refactor CustomCc class and add getMethodCode method
    Refactor CustomCc class to extend Mage_Payment_Block_Form instead of Mage_Payment_Block_Form_Abstract and add method for retrieving method code.

--- CustomCc.php
+++ CustomCc.php
@@ -1,11 +1,20 @@
 <?php
 
-class Eshop_CustomPayment_Block_Form_CustomCc extends Mage_Payment_Block_Form_Abstract
+class Eshop_CustomPayment_Block_Form_CustomCc extends Mage_Payment_Block_Form
 {
     protected function _construct()
     {
         parent::_construct();
 
         $this->setTemplate('eshop/custompayment/form/cc.phtml');
+    }
+    public function getMethodCode()
+    {
+        return 'custompayment_method';
+    }
+
+    protected function _toHtml()
+    {
+
     }
 }
