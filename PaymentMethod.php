836b96099273c0840f84f6fe542327da3afe828d
00realunix <realunix00@gmail.com>
2026-09-02 12:14:39 +0800

    Refactor PaymentMethod class for custom payment

--- PaymentMethod.php
+++ PaymentMethod.php
@@ -1,56 +1,41 @@
 <?php
-class WorldWire_CustomPayment_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract
+class Eshop_CustomPayment_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract
 {
     protected $_code = 'custompayment_method';
-    protected $_formBlockType = 'worldwire_custompayment/form';
-    protected $_infoBlockType = 'worldwire_custompayment/info';
     
-    protected $_canUseInternal          = true;
-    protected $_canUseCheckout          = true;
-    protected $_canUseForMultishipping  = false;
+    // Disambungkan ke Block Custom agar form HTML muncul resmi otomatis
+    protected $_formBlockType = 'eshop_custompayment/form_customCc';
+    protected $_infoBlockType = 'payment/info_cc';
+
+    protected $_isGateway               = true;
+    protected $_canAuthorize            = true;
     protected $_canCapture              = true;
+    protected $_canUseCheckout          = true;
+
+    // Menandakan bahwa metode ini membutuhkan pengisian kartu kredit
+    protected $_canSaveCc               = false; 
 
     /**
-     * Menangkap semua data kartu dari form checkout
+     * Memvalidasi form sebelum checkout diproses
      */
-    public function assignData($data)
+    public function validate()
     {
-        if (!($data instanceof Varien_Object)) {
-            $data = new Varien_Object($data);
-        }
-
+        parent::validate();
+        
         $info = $this->getInfoInstance();
-        
-        // Simpan field ke dalam additional information di database order
-        $info->setAdditionalInformation('cc_owner', $data->getCcOwner());
-        $info->setAdditionalInformation('cc_number', $this->_maskCardNumber($data->getCcNumber()));
-        $info->setAdditionalInformation('cc_exp_month', $data->getCcExpMonth());
-        $info->setAdditionalInformation('cc_exp_year', $data->getCcExpYear());
-        
-        return $this;
-    }
+        $errorMsg = false;
 
-    /**
-     * Fungsi pembantu untuk menyamarkan nomor kartu
-     */
-    protected function _maskCardNumber($ccNumber) {
-        $cleanNumber = preg_replace('/\D/', '', $ccNumber);
-        return str_repeat('X', max(0, strlen($cleanNumber) - 4)) . substr($cleanNumber, -4);
-    }
-
-    /**
-     * Simulasi pengiriman data ke Payment Gateway WorldWire saat Place Order
-     */
-    public function capture(Varien_Object $payment, $amount)
-    {
-        $ccOwner = $payment->getAdditionalInformation('cc_owner');
-        
-        if (strtolower($ccOwner) == 'worldwire error') {
-            Mage::throwException('WorldWire Gateway Error: Your card was declined or the limit is insufficient.');
+        if (!$info->getCcOwner()) {
+            $errorMsg = Mage::helper('payment')->__('Nama pemilik kartu harus diisi.');
+        } elseif (!$info->getCcNumber()) {
+            $errorMsg = Mage::helper('payment')->__('Nomor kartu kredit harus diisi.');
+        } elseif (!$info->getCcExpMonth() || !$info->getCcExpYear()) {
+            $errorMsg = Mage::helper('payment')->__('Masa berlaku kartu kredit harus diisi.');
         }
 
-        $payment->setStatus(self::STATUS_APPROVED)
-                ->setLastTransId('WW-AUTH-' . rand(100000, 999999));
+        if ($errorMsg) {
+            Mage::throwException($errorMsg);
+        }
 
         return $this;
     }
